baf2d01f5cf015b8554fdb4b44a637d4075e25fb
cesar madera <camgcamg11@gmail.com>
2021-09-29 16:23:24 -0500

    done

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,45 @@
+class FlatsController < ApplicationController
+  before_action :find_flat, only: [:show, :destroy, :edit, :update]
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params[:flat].permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+end
